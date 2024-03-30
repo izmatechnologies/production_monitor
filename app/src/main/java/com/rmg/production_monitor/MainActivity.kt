b35d6ce407f889fc1b79ab13629262df4b469736
Faisal Mohammad Nirzon <nirzon192@gmail.com>
2024-03-31 01:46:22 +0600

    initial commit

---
+++ app/src/main/java/com/rmg/production_monitor/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rmg.production_monitor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
