f81b917de4d9056c9606e98895b5fb3b70d7c3b3
Lenovo <rakib.cse99@gmail.com>
2024-04-23 14:24:40 +0600

    Login UI design

--- app/src/main/java/com/rmg/production_monitor/LoginFragment.kt
+++ app/src/main/java/com/rmg/production_monitor/LoginFragment.kt
@@ -1,23 +1,20 @@
 package com.rmg.production_monitor
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
-class DashBoardFragment : Fragment() {
 
+class LoginFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dash_board, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
-
 
 }
