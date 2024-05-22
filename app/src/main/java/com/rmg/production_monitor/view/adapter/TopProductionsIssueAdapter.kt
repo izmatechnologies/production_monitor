a9ab2cad9102d74648b8dd834cc82fd902e744f5
Asus <pavel@easital.com>
2024-05-22 14:28:00 +0600

    heatmap fragment update

--- app/src/main/java/com/rmg/production_monitor/view/adapter/TopProductionsIssueAdapter.kt
+++ app/src/main/java/com/rmg/production_monitor/view/adapter/TopProductionsIssueAdapter.kt
@@ -2,7 +2,6 @@ package com.rmg.production_monitor.view.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
